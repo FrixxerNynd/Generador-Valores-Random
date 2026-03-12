2594b0e0916cb9aa0d876d8285470934281d46b1
Cesar Sarmiento <169074465+TakeaKSV@users.noreply.github.com>
2026-03-11 23:18:32 -0600

    Delete casino-virtual/backend/src directory

--- casino-virtual/backend/src/juegos/interface/dto/spin-roulette.dto.ts
+++
@@ -1,9 +0,0 @@
-export class SpinRouletteDto {
-  userId!: string;
-  selectedNumber!: number;
-  minRange!: number;
-  maxRange!: number;
-  betAmount!: number;
-  payoutMultiplier?: number;
-  gameDescription?: string;
-}
