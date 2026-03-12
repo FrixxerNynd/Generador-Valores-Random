2b721d18caf2b4b42164c95e029d6e623ed7de83
Cesar Sarmiento <169074465+TakeaKSV@users.noreply.github.com>
2026-03-11 22:35:51 -0600

    Delete src directory

--- src/app.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RouletteModule } from './modules/roulette/roulette.module';
-
-@Module({
-  imports: [RouletteModule],
-})
-export class AppModule {}
